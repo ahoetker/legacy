90c5abad18603f75ef3402583c129d240abe5572
ahoetker <ahoetker@me.com>
2018-10-26 02:19:03 -0700

    small changes to routes

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from flask import (
 from app import app
 from app.forms import SeriesSearchForm
 from legacy.util import util_get_series_data, process
-from legacy.plots import inline_html
+from legacy.plots import inline_html, outfile_html
 import asyncio
 
 
@@ -35,45 +35,13 @@ def index():
             except ValueError as e:
                 flash("No valid results found for {}".format(query), "error")
                 return(redirect(url_for("index")))
-            html_file = inline_html(df, show_title)
-            with open(html_file, "r") as h:
-                html = h.read()
-            return html
+            # html_file = outfile_html(df, show_title)
+            # with open(html_file, "r") as h:
+            #     html = h.read()
+            # return html
+            script, div = inline_html(df, show_title)
+            return render_template("index.html", form=form, script=script, div=div)
         else:
             flash("No valid results found for {}.".format(query), "error")
             return redirect(url_for("index"))
-        # try:
-        #     series_data = util_get_series_data(query)
-        #     if series_data.get("valid") is False:
-        #
-        #     show_title = series_data["show_title"]
-        #     raw_data = series_data["raw_data"]
-        #     df = process(raw_data)
-        #     html_file = inline_html(df, show_title)
-        #     with open(html_file, "r") as h:
-        #         html = h.read()
-        #     return html
-        # except KeyError as e:
-        #     flash("No valid results found for query {}.".format(query), "error")
-        #     return redirect(url_for("index"))
-    return render_template("index.html", title="Legacy", form=form, plot_html=None)
-
-
-@app.route("/seriessearch", methods=["GET", "POST"])
-def seriessearch():
-    form = SeriesSearchForm(validators=["DataRequired()"])
-    if form.validate_on_submit():
-        data = form.data.copy()
-        query = data.get("query")
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        series_data = util_get_series_data(query)
-        show_title = series_data["show_title"]
-        raw_data = series_data["raw_data"]
-        df = process(raw_data)
-        html_file = inline_html(df, show_title)
-        with open(html_file, "r") as h:
-            html = h.read()
-        return html
-        # return render_template("search.html", title="Search", form=form, plot_html=str(html_file))
-    return render_template("search.html", title="Search", form=form, plot_html=None)
+    return render_template("index.html", title="Legacy", form=form)
--- legacy/plots.py
+++ legacy/plots.py
@@ -3,7 +3,7 @@ from bokeh.models import ColumnDataSource, OpenURL, TapTool
 from bokeh.models.tools import HoverTool
 from bokeh.palettes import Dark2_5 as palette
 from bokeh.resources import CDN
-from bokeh.embed import file_html
+from bokeh.embed import file_html, components
 from legacy.root_logger import get_logger
 from legacy.data_processing import clean_df
 from pathlib import Path
@@ -101,7 +101,7 @@ def seasons_plot(df: pd.core.frame.DataFrame, show_title: str):
     show(p)
 
 
-def inline_html(df: pd.core.frame.DataFrame, show_title: str) -> str:
+def outfile_html(df: pd.core.frame.DataFrame, show_title: str) -> str:
         logger.info("Plotting series data (fancy plot)")
         outfile = Path(__file__).parent.parent / "local_data" / "plot.html"
         output_file(outfile, mode="inline")
@@ -153,3 +153,55 @@ def inline_html(df: pd.core.frame.DataFrame, show_title: str) -> str:
 
         save(p)
         return(Path(outfile))
+
+
+def inline_html(df: pd.core.frame.DataFrame, show_title: str) -> str:
+        logger.info("Plotting series data (fancy plot)")
+
+        # Each season gets its own color
+        colors = itertools.cycle(palette)
+        colormap = {season: color for season, color in zip(df.Season.unique(), colors)}
+        df["colors"] = [colormap[x] for x in df.Season]
+
+        # Source df to Bokeh
+        source = ColumnDataSource(df)
+
+        # Assemble figure
+        p = figure(
+            title="IMDb Ratings for {}".format(show_title),
+            x_axis_label="Episode Number",
+            y_axis_label="IMDb Rating",
+            plot_height=600,
+            plot_width=1200,
+            tools="tap",
+        )
+        p.circle(
+            x="Sequential",
+            y="imdbRating",
+            source=source,
+            size=10,
+            color="colors",
+            alpha=0.75,
+        )
+        p.line(
+            x="Sequential",
+            y="imdbRating",
+            source=source,
+            color="black",
+            alpha=0.75,
+            line_width=1,
+        )
+        hover = HoverTool()
+        hover.tooltips = [
+            ("Title", "@Title"),
+            ("Season", "@Season"),
+            ("Episode", "@Episode"),
+            ("Rating", "@imdbRating"),
+        ]
+        p.add_tools(hover)
+        url = "https://imdb.com/title/@imdbID/"
+        taptool = p.select(type=TapTool)
+        taptool.callback = OpenURL(url=url)
+
+        script, div = components(p)
+        return (script, div)
